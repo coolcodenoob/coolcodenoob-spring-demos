0a6978921b7fcae82a504091b42d15ba9b288ca5
coolcodenoob <linchunjie007@gmail.com>
2024-03-25 16:42:44 +0800

    add content about springboot-banner

---
+++ springboot-banner/src/test/java/top/coolcodenoob/springbootbanner/SpringbootBannerApplicationTests.java
@@ -0,0 +1,13 @@
+package top.coolcodenoob.springbootbanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
